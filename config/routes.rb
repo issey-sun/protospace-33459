96c15f9dad25e05c706b4bdb598274d394dd7bec
issey-sun <76093901+issey-sun@users.noreply.github.com>
2020-12-31 16:27:03 +0900

    コントローラーを作成し、アクションとルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
   resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
