e23be283e05c5036818be4d3013f52782aa4f265
issey-sun <76093901+issey-sun@users.noreply.github.com>
2021-01-03 14:21:27 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+#   devise_for :users
+#   get 'prototypes/index'
+#   root to: "prototypes#index"
+#   resources :prototypes do
+#     resources :comments, only: :create
+#   end
+#   resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+# end
+
+devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes do
+  resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy,] do
     resources :comments, only: :create
   end
-  resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  resources :users, only: :show
+  
 end
