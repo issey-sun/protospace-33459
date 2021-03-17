8de074fbd42f7c59d4549e0a49a9f830001c7703
issey-sun <76093901+issey-sun@users.noreply.github.com>
2021-03-17 13:51:07 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy,] do
+  resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
 
